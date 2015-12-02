3e73dd698ea6d80a2aaa532a820f36a2564104f6
musienkoyuriy <musienkoyuriy@gmail.com>
2015-12-02 18:28:15 +0200

    2 spaces indent instead of 4

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,5 +2,5 @@ import { createStore } from 'redux';
 import reducer from '../reducers/index';
 
 export default function configureStore(initialState) {
-	return createStore(reducer, initialState);
+  return createStore(reducer, initialState);
 }
