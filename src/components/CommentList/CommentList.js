91f3c932971c2c749ee2b2ce93423af38ba41288
musienkoyuriy <musienkoyuriy@gmail.com>
2015-12-09 13:41:54 +0200

    convert to the 2 spaces

--- src/components/CommentList/CommentList.js
+++ src/components/CommentList/CommentList.js
@@ -6,19 +6,19 @@ import styles from './commentlist.css';
 
 let CommentList = (props, context) => {
   return (
-      <div styleName="comment-list">
-        {props.comments.map((comment, i) => {
-          return <Comment commentData={comment} key={i} />
-        })}
-      </div>
-    );
+    <div styleName="comment-list">
+      {props.comments.map((comment, i) => {
+        return <Comment commentData={comment} key={i} />
+      })}
+    </div>
+  );
 }
 CommentList.propTypes = {
-	comments: PropTypes.arrayOf(PropTypes.shape({
-		author: PropTypes.string.isRequired,
-		date: PropTypes.string.isRequired,
-		commentText: PropTypes.string.isRequired
-	})).isRequired
+  comments: PropTypes.arrayOf(PropTypes.shape({
+    author: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    commentText: PropTypes.string.isRequired
+  })).isRequired
 };
 
 export default CSSModules(CommentList, styles);
